57c4d892f4e0e4a4a24f8163b6c32e8482945339
Daria Malina <gluto68@gmail.com>
2021-09-20 20:09:45 +0300

    fix: refactor name

--- src/Numbers, strings/NicknameGenerator.js
+++ src/Numbers, strings/NicknameGenerator.js
@@ -1,10 +1,9 @@
-// eslint-disable-next-line consistent-return
 function nicknameGenerator(name) {
-  const vowel = ['a', 'e', 'i', 'o', 'u'];
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
   if (name.length <= 3) {
     return 'Error: Name too short';
   }
-  if (!vowel.includes(name[2])) {
+  if (!vowels.includes(name[2])) {
     return name.slice(0, 3);
   }
   return name.slice(0, 4);
