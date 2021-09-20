a022de4d9f394fdec5fd727bc81c63a8b2d8bf34
Daria Malina <gluto68@gmail.com>
2021-09-20 16:05:51 +0300

    fix: formatting

--- src/Numbers, strings/AbsentVowel.js
+++ src/Numbers, strings/AbsentVowel.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line consistent-return
 function absentVowel(x) {
-  const array = ['a', 'e', 'i', 'o', 'u'];
-  const arr = x.split('');
-  for (let i = 0; i < array.length; i += 1) {
-    if (!arr.includes(array[i])) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const arr = x.toLowerCase().split('');
+  for (let i = 0; i < vowels.length; i += 1) {
+    if (!arr.includes(vowels[i])) {
       return i;
     }
   }
